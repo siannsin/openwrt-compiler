15a5ed16932611b69e066c11a4d4d8ca7fcc40f9
Grinch27 <Grinch27@outlook.com>
2024-04-05 17:54:39 +0800

    update support for old luci

--- openwrt/diy-part2.sh
+++ openwrt/diy-part2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Old Luci
+echo "
+CONFIG_PACKAGE_luci-compat=y
+" >> .config
+
 # Add luci-app-adguardhome
 rm -rf package/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luci-app-adguardhome
