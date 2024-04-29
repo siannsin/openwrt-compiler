62b0071a0171f3e0cf380ef69b0f4606e6d47889
Grinch27 <Grinch27@outlook.com>
2024-04-29 23:45:09 +0800

    add iperf3 & batman-adv

--- openwrt/diy-part2.sh
+++ openwrt/diy-part2.sh
@@ -30,6 +30,22 @@ CONFIG_PACKAGE_luci-app-squid=y
 CONFIG_PACKAGE_squid=y
 " >> .config
 
+# iperf3
+echo "
+CONFIG_PACKAGE_iperf=n
+CONFIG_PACKAGE_iperf3=y
+CONFIG_PACKAGE_iperf3-ssl=n
+" >> .config
+
+# batman-adv
+echo "
+CONFIG_PACKAGE_luci-proto-batman-adv=y
+CONFIG_PACKAGE_kmod-batman-adv=y
+CONFIG_PACKAGE_batctl-default=n
+CONFIG_PACKAGE_batctl-full=y
+CONFIG_PACKAGE_batctl-tiny=n
+" >> .config
+
 # ---------- sync config ----------
 make oldconfig
 cat ./.config
