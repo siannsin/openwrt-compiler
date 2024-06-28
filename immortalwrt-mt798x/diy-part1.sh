f2e47c6d8b9db2edcb75f55cf0c0080c3e0bfb13
Siann Sin <9166272+siannsin@users.noreply.github.com>
2024-06-28 13:51:15 +0800

    Update diy-part1.sh

--- immortalwrt-mt798x/diy-part1.sh
+++ immortalwrt-mt798x/diy-part1.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
