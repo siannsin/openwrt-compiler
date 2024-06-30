c8e3450d412ef268257745d66086ed4af0c6e54a
Siann Sin <9166272+siannsin@users.noreply.github.com>
2024-06-30 20:41:42 +0800

    Update diy-half.sh

--- immortalwrt-mt798x/diy-half.sh
+++ immortalwrt-mt798x/diy-half.sh
@@ -1,2 +1,2 @@
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
